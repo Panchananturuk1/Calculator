477dd40f8837953dcb02bc608212e4e4601075a2
Panchananturuk1 <pturuk123@gmail.com>
2022-06-02 11:42:51 +0530

    complte

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello!')
+//function that display value
+
+function disp(val) {
+  document.getElementById('inp').value += val;
+}
+
+//function that evaluates the digit and return result
+function solve() {
+  let x = document.getElementById('inp').value;
+  let y = eval(x);
+  document.getElementById('inp').value = y;
+}
+
+function clr() {
+  document.getElementById('inp').value = "";
+}
